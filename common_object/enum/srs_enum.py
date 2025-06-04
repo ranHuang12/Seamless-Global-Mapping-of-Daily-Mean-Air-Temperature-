09221aefa679945a84d790563a04068c93ef69a6
Huang R. <ran_huang@hdu.edu.cn>
2025-06-04 11:13:29 +0800

    Delete common_object directory

--- common_object/enum/srs_enum.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class SRSEnum(Enum):
-    WGS84 = "EPSG:4326"
-    WGS84_NUM = 4326
--- common_object/enum/type_enum.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-from osgeo import gdalconst
-
-
-class TypeEnum(Enum):
-    TEMPERATURE = gdalconst.GDT_Int32
