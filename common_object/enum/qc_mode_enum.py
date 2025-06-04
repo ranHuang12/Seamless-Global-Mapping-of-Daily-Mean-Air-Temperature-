42e1ff7b7ed9490bce15ccc2a6b8b52b2b41cb7b
Huang R. <ran_huang@hdu.edu.cn>
2025-06-04 16:07:44 +0800

    Delete common_object directory

--- common_object/enum/qc_mode_enum.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-from common_object.entity.qc_mode import QcMode
-
-
-class QcModeEnum(Enum):
-    GOOD_QUALITY = QcMode("goodquality", "GQ")
-    OTHER_QUALITY = QcMode("orderquality", "OQ")
-    ALL = QcMode("all", "ALL")
